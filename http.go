8f3d9ef3155e6a9138eaf78d6629b8e3eef3ef95
Will Conant <will.conant@gmail.com>
2010-05-03 10:51:10 -0600

    getting closer (probably doesn't work yet)

--- http.go
+++ http.go
@@ -1,8 +1,14 @@
 package msglite
 
 import (
+	"net"
 	"bytes"
 	"http"
+	"io/ioutil"
+	"strings"
+	"strconv"
+	"bufio"
+	"os"
 )
 
 type HttpServer struct{
@@ -19,41 +25,89 @@ func NewHttpServer(exchange *Exchange, listenNetwork string, listenAddress strin
 
 func (server *HttpServer) Run() {
 	handler := func(conn *http.Conn, req *http.Request) {
-		var msgBuf bytes.Buffer
-		
-		msgBuf.WriteString(req.Method + "\n")
-		msgBuf.WriteString(req.RawURL + "\n")
-		msgBuf.WriteString(req.Proto + "\n")
-		msgBuf.WriteString("Host: " + req.Host + "\n")
-		msgBuf.WriteString("User-Agent: " + req.UserAgent + "\n")
-		msgBuf.WriteString("Referer: " + req.Referer + "\n")
-		
-		for k, v := range(req.Header) {
-			msgBuf.WriteString(k + ": " + v + "\n")
-		}
-		
-		msgBuf.WriteString("\n")
-		
-		var bodyBuf bytes.Buffer
-		var inBuf [4096]byte
-		
-		for {
-			nr, err := req.Body.Read(&inBuf)
-			
-			if nr > 0 {
-				bodyBuf.Write(inBuf[0:nr])
-			}
-			
-			if err == os.EOF {
-				break
-			}
-			
-			if err != nil {
-				// TODO: make this more graceful
-				panic("error reading http request body")
-			}
-		}
-		
-		msgBuf.
+		server.handleReq(conn, req)
 	}
+	
+	listener, err := net.Listen(server.listenNetwork, server.listenAddress)
+	if err != nil {
+		panic(err)
+	}
+	
+	http.Serve(listener, http.HandlerFunc(handler))
+}
+
+func (server *HttpServer) handleReq(conn *http.Conn, req *http.Request) {
+	msgBody, err := server.transformReq(req)
+	if err != nil {
+		http.Error(conn, err.String(), http.StatusInternalServerError)
+	}
+	
+	reply := server.exchange.SendQuery(server.exchangeToAddress, msgBody)
+	
+	server.transformReply(reply, conn)
+}
+
+func (server *HttpServer) transformReq(req *http.Request) (string, os.Error) {
+	var msgBuf bytes.Buffer
+	
+	msgBuf.WriteString(req.Method + " " + req.RawURL + " " + req.Proto + "\n")
+	msgBuf.WriteString("Host: " + req.Host + "\n")
+	msgBuf.WriteString("User-Agent: " + req.UserAgent + "\n")
+	msgBuf.WriteString("Referer: " + req.Referer + "\n")
+	
+	for k, v := range(req.Header) {
+		msgBuf.WriteString(k + ": " + v + "\n")
+	}
+	
+	msgBuf.WriteString("\n")
+	
+	bodySlice, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return "", err
+	}
+	
+	msgBuf.Write(bodySlice)
+	
+	return msgBuf.String(), nil
+}
+
+func (server *HttpServer) transformReply(msg Message, conn *http.Conn) {
+	reader := bufio.NewReader(strings.NewReader(msg.Body))
+	
+	statusLine, err := reader.ReadString('\n')
+	if err != nil {
+		goto BadResponse
+	}
+	
+	status, err := strconv.Atoi(statusLine[0:len(statusLine)-1])
+	if err != nil {
+		goto BadResponse
+	}
+	
+	for {
+		headerLine, err := reader.ReadString('\n')
+		if err != nil {
+			goto BadResponse
+		}
+		
+		if headerLine == "\n" {
+			break
+		}
+		
+		sepIdx := strings.Index(headerLine, ": ")
+		header := headerLine[0:sepIdx]
+		value := headerLine[sepIdx+2:len(headerLine)-1]
+
+		conn.SetHeader(header, value)
+	}
+	
+	bodySlice, _ := ioutil.ReadAll(reader)
+	
+	conn.WriteHeader(status)
+	conn.Write(bodySlice)
+	
+	return
+	
+BadResponse:
+	http.Error(conn, "bad response from upstream handler", http.StatusInternalServerError)
 }
